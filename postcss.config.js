d7a150fc89d8f9b6a08f7ca4da668957b08f380c
Jeffrey Andrew Agregado <jeffagregado@gmail.com>
2022-04-20 21:07:17 +0800

    postcss.config: change broserlist

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: [
     // require('postcss-import'),
     require('postcss-preset-env')({
-      browsers: 'last 2 versions',
+      browsers: 'last 2 versions, > 0.5%, IE >= 10',
       stage: 1,
       features: {
         'nesting-rules': true,
