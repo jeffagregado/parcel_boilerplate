21a5ad3c41e2a527b1bbc4faf826b2b7ee833efc
Jeffrey Andrew Agregado <jeffagregado@gmail.com>
2022-04-20 21:26:11 +0800

    postcss.config: change config

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,27 @@
+const postcssPresetEnv = require('postcss-preset-env')
+
 module.exports = {
   plugins: [
     // require('postcss-import'),
-    require('postcss-preset-env')({
+    // require('postcss-preset-env')({
+    //   browsers: 'last 2 versions, > 0.5%, IE >= 10',
+    //   stage: 1,
+    //   features: {
+    //     'nesting-rules': true,
+    //   },
+    //   autoprefixer: {
+    //     grid: true,
+    //   }
+    // }),
+    postcssPresetEnv({
       browsers: 'last 2 versions, > 0.5%, IE >= 10',
-      stage: 1,
+      stage: 2,
       features: {
         'nesting-rules': true,
       },
       autoprefixer: {
         grid: true,
-      }
+      },
     }),
   ],
-};
+}
