0292352e4e3c294e5dee84c627c4080e784d458e
Jeffrey Andrew Agregado <jeffagregado@gmail.com>
2022-04-21 20:18:35 +0800

    test config

---
+++ src/js/index.ts
@@ -0,0 +1,14 @@
+import LazyLoad from "vanilla-lazyload";
+
+// @ts-ignore
+if (document.documentMode) { 
+  window.alert('This is IE')
+}
+
+const myLazyLoad = new LazyLoad();
+// After your content has changed...
+myLazyLoad.update();
+
+const test = <HTMLImageElement>document.getElementById('test');
+
+test.src = '../img/Screenshot.png'
